7e122894182094f30dd3ef301e3ff88dde4472cd
Mathieu Layeillon <mathieu.layeillon@eleve.isep.fr>
2021-01-22 09:27:30 +0100

    reccuperation des jetons

--- src/jetons/JetonEchanger.java
+++ src/jetons/JetonEchanger.java
@@ -15,7 +15,7 @@ import javafx.scene.image.ImageView;
 public class JetonEchanger extends Jetons {
     Scanner scanner = new Scanner(System.in);
     String nom;
-	public static Image im = new Image("file:images/actions/Jeton1-Face1.png", 50, 50, false, false);
+	public static Image im = new Image("file:images/actions/Jeton1-Face1.png", 50, 50, false, false); // Image du bouton échanger les tuiles
 	public static ImageView imView = new ImageView(im);
 
     public JetonEchanger(String nom) {
@@ -24,41 +24,26 @@ public class JetonEchanger extends Jetons {
 
     @Override
     public void action(Detectives[] listeDetectives, District[] board, int jActuel) {
-       /* System.out.println(
-                "Quelle est la 1ere tuile que vous voulez echanger (entrez l'abscisse puis l'ordonnee de la tuile (entre 1 et 3)");
-        int abscisse = scanner.nextInt() - 1;
-        int ordonnee = scanner.nextInt() - 1;
-        int coordonnee = 3 * abscisse + ordonnee;
-        System.out.println(
-                "Quelle est la 2eme tuile que vous voulez Ã©changer (entrez l'abscisse puis l'ordonnÃ©e de la tuile (entre 1 et 3))");
-
-        int abscisse2 = scanner.nextInt() - 1;
-        int ordonnee2 = scanner.nextInt() - 1;
-        int coordonnee2 = 3 * abscisse2 + ordonnee2;
-        District temp = board[coordonnee];
-        board[coordonnee] = board[coordonnee2];
-        board[coordonnee2] = temp;
-        */
 		for (int i = 0; i < 9; i++) {
-			Node imTuile1 = InterfaceGraphique.tuile[i].getGraphic();
-			final int indice1 = i;
+			Node imTuile1 = InterfaceGraphique.tuile[i].getGraphic(); // On garde en mémoire l'image de la première tuile cliquée
+			final int indice1 = i; 
 			InterfaceGraphique.tuile[i].setOnAction(new EventHandler<ActionEvent>() {
 				public void handle(ActionEvent e) {
-					InterfaceGraphique.tuile[indice1].setDisable(true);
+					InterfaceGraphique.tuile[indice1].setDisable(true); // il est impossible d'échanger une tuile avec elle même
 					for (int j = 0; j < 9; j++) {
-						Node imTuile2 = InterfaceGraphique.tuile[j].getGraphic();
+						Node imTuile2 = InterfaceGraphique.tuile[j].getGraphic(); // on garde en mémoire la seconde image
 						final int indice2 = j;
 						InterfaceGraphique.tuile[j].setOnAction(new EventHandler<ActionEvent>() {
 							public void handle(ActionEvent e) {
-								InterfaceGraphique.tuile[indice2].setGraphic(imTuile1);
+								InterfaceGraphique.tuile[indice2].setGraphic(imTuile1); // on échange les images des deux tuiles 
 								InterfaceGraphique.tuile[indice1].setGraphic(imTuile2);
 								District temp = Jeu.board[indice1];
 								Jeu.board[indice1] = Jeu.board[indice2];
 								Jeu.board[indice2] = temp;
-								InterfaceGraphique.tuile[indice1].setDisable(false);
-								InterfaceGraphique.finAction();
+								InterfaceGraphique.tuile[indice1].setDisable(false); // on réactive les boutons des tuiles
+								InterfaceGraphique.finAction(); // passage à l'action suivante
 								for (int h = 0; h < 9; h++) {
-									InterfaceGraphique.tuile[h].setOnAction(new EventHandler<ActionEvent>() {
+									InterfaceGraphique.tuile[h].setOnAction(new EventHandler<ActionEvent>() { // permet d'empecher de faire un autre échnage de tuile
 										@Override
 										public void handle(ActionEvent e) {
 
@@ -75,6 +60,7 @@ public class JetonEchanger extends Jetons {
 		}
     }
 
+    // Getters and Setters
     @Override
     public String getNom() {
         return nom;
--- src/jetons/JetonTourner.java
+++ src/jetons/JetonTourner.java
@@ -15,9 +15,8 @@ import javafx.scene.image.ImageView;
 public class JetonTourner extends Jetons {
 	Scanner scanner = new Scanner(System.in);
 	String nom;
-	public static Image im = new Image("file:images/actions/Jeton1-Face2.png", 50, 50, false, false);
+	public static Image im = new Image("file:images/actions/Jeton1-Face2.png", 50, 50, false, false); // on d�finit l'image du jeton tourner
 	public static ImageView imView = new ImageView(im);
-	// private static HelloApp interfaceG = new HelloApp();
 
 	public JetonTourner() {
 	}
@@ -28,26 +27,6 @@ public class JetonTourner extends Jetons {
 
 	@Override
 	public void action(Detectives[] listeDetectives, District[] board, int jActuel) {
-		/*// HelloApp.idEnCours = "";
-		System.out.println("Entrez l'abscisse puis l'ordonnée de la tuile que vous voulez tourner:");
-		/*
-		 * if (scanner.nextLine().equals("")) { while(!HelloApp.idEnCours.substring(0,
-		 * 1).equals("Q")) { //Thread.sleep((long) 1); }
-		 * board[Integer.parseInt(HelloApp.idEnCours.substring(1, 2))].setMur();
-		 * System.out.println("La tuile " +
-		 * Integer.parseInt(HelloApp.idEnCours.substring(1, 2)) + " a tourné"); } else {
-		 
-		int abscisse = scanner.nextInt() - 1;
-		int ordonnee = scanner.nextInt() - 1;
-		int coordonnee = 3 * abscisse + ordonnee;
-		System.out.println("Appuyez sur <entrer> puis tapper 'stop' quand la disposition vous convient");
-		scanner.nextLine();
-		do {
-			board[coordonnee].setMur();
-			// interfaceG.printBoardInterface();
-			// TableauTuiles.printBoardConsole(board);
-		} while (!scanner.nextLine().equals("stop"));
-		// }*/
 		InterfaceGraphique.root.add(InterfaceGraphique.validerB, 6, 1);
 		for (int i = 0; i < 9; i++) {
 			InterfaceGraphique.tuile[i].setDisable(false);
@@ -86,6 +65,7 @@ public class JetonTourner extends Jetons {
 		}
 	}
 
+	// Getters
 	@Override
 	public String getNom() {
 		return nom;
